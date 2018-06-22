923b83d1968ea93f9af6d56c29e4c1d7ff73f3a6
Jonathan Kim <jonkim204@gmail.com>
2018-06-22 14:17:15 -0700

    fixed some issues

--- scripts/api.js
+++ scripts/api.js
@@ -1,20 +1,13 @@
 'use strict';
 /* global  */
 
-/*
-* retrieve list of bookmarks from api
-* create a new bookmark
-* delete a bookmark
-*/
+
 
 const api = function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jon';
 
   const getBookmarks = function(callback) {
-    // GET all store items from API
-
-    // AJAX GET doesn't need a dataType field.
-    // GET is also the default method
+  
     $.ajax({
       url: `${BASE_URL}/bookmarks`,
       method: 'GET',
@@ -23,7 +16,7 @@ const api = function() {
   };
 
   const createBookmark = function (name, callback) {
-    // POST store item to API
+    
     const newBookmark = JSON.stringify(name);
 
     $.ajax({
@@ -36,7 +29,7 @@ const api = function() {
   };
 
   const deleteBookmark = function (id, callback) {
-    // DELETE store item from API
+    
 
     $.ajax({
       url: `${BASE_URL}/bookmarks/${id}`,
--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,6 @@
 
 
 $(document).ready(function () {
-  // call to the API and get the current data. 
   bookmarkActions.bindEventListeners();
   api.getBookmarks((bookmarks) => {
     bookmarks.forEach(bookmark => store.addBookmark(bookmark));
